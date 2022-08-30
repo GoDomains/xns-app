1f8e9aed077ce8c1258a5aa97a83dc4c11a82d7a
aditi27-rajshail <aditirajankhot.7@gmail.com>
2022-08-30 19:22:48 +0530

    Home uncomment

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -64,9 +64,7 @@ const TagLine = styled('div')`
   display: inline-block;
   font-size: 52px;
   width: 500px;
-  text-align: center;
-  margin-top: 20px;
-  align-self: center;
+  align-self: flex-end;
 `
 const NetworkLogoGrid = styled('div')`
   display: flex;
@@ -74,9 +72,6 @@ const NetworkLogoGrid = styled('div')`
 const MidGrid = styled('div')`
   margin-bottom: 10px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
 `
 
 const NetworkStatus = styled('div')`
@@ -347,21 +342,21 @@ export default ({ match }) => {
   const { url } = match
   const { t } = useTranslation()
 
-  // const {
-  //   data: { accounts }
-  // } = useQuery(GET_ACCOUNT)
+  const {
+    data: { accounts }
+  } = useQuery(GET_ACCOUNT)
 
-  // const {
-  //   data: { network, displayName, isReadOnly, isSafeApp }
-  // } = useQuery(HOME_DATA, {
-  //   variables: { address: accounts?.[0] }
-  // })
+  const {
+    data: { network, displayName, isReadOnly, isSafeApp }
+  } = useQuery(HOME_DATA, {
+    variables: { address: accounts?.[0] }
+  })
 
   return (
     <HeroBGC>
       <Hero>
         <HeroTop>
-          {/* <NetworkLogoGrid>
+          <NetworkLogoGrid>
             <NetworkStatus>
               <Network>
                 {`${network} ${t('c.network')}`}
@@ -380,8 +375,8 @@ export default ({ match }) => {
               )}
             </NetworkStatus>
             <IconLogo src={goDomainsLogo} />
-          </NetworkLogoGrid> */}
-          {/* <Nav>
+          </NetworkLogoGrid>
+          <Nav>
             {accounts?.length > 0 && !isReadOnly && (
               <NavLink
                 active={url === '/address/' + accounts[0]}
@@ -390,10 +385,13 @@ export default ({ match }) => {
                 {t('c.mynames')}
               </NavLink>
             )}
-            <NavLink to="/favourites">{t('c.favourites')}</NavLink>
+            <NavLink to="/faq">{t('c.faq')}</NavLink>
+            {/*  <NavLink to="/favourites">{t('c.favourites')}</NavLink>  */}
             <NavLink to="/about">{t('c.about')}</NavLink>
-              <ExternalLink href={aboutPageURL()}>{t('c.about')}</ExternalLink>
-          </Nav> */}
+            <NavLink to="/terms-of-service">Terms of Service</NavLink>
+
+            {/*   <ExternalLink href={aboutPageURL()}>{t('c.about')}</ExternalLink> */}
+          </Nav>
           {/* <MainPageBannerContainer>
           <DAOBannerContent />
         </MainPageBannerContainer> */}
@@ -407,20 +405,20 @@ export default ({ match }) => {
             alt="ENS logo"
           /> */}
             <MidGrid>
+              <TagLine>{t('c.tagLine')}</TagLine>
               <Rocket
                 initial={animation.initial}
                 animate={animation.animate}
-                src={goDomainsLogo}
+                src={rocket}
                 alt="Rocket"
               />
-              <TagLine>We will be live soon</TagLine>
             </MidGrid>
 
             {/*   <PermanentRegistrarLogo
             initial={animation.initial}
             animate={animation.animate}
           /> */}
-            {/* <Search /> */}
+            <Search />
           </>
         </SearchContainer>
       </Hero>
